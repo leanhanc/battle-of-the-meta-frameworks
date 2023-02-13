6e4afd329acc4d2b722b740fbff48cbac2251418
leo10099 <leandrohanc@gmail.com>
2023-02-13 01:00:51 -0300

    feat(astro): add astro

---
+++ astro/astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  outDir: "build",
+  integrations: [tailwind(), react()]
+});
---
+++ astro/tailwind.config.cjs
@@ -0,0 +1,34 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			container: {
+				center: true,
+				padding: {
+					DEFAULT: "1rem",
+				},
+			},
+			fontFamily: {
+				sans: ["Inter", ...defaultTheme.fontFamily.sans],
+				heading: ["OpenSans", ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				yellow: {
+					100: "#FFFBEA",
+					200: "#FFF3C4",
+					300: "#FFEA76",
+					400: "#FFD940",
+					500: "#F6E092",
+					600: "#D4B106",
+					700: "#9C7B00",
+					800: "#744800",
+					900: "#4D2600",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
